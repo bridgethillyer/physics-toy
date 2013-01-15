10a17608565394c0b6d5952cd72c1ec93d6c7213
Bridget Hillyer <bridget.hillyer@gmail.com>
2013-01-14 19:30:36 -0500

    Initial commit

---
+++ gen/R.java.d
@@ -0,0 +1,7 @@
+/Users/bridgethillyer/learning/android/box2d-basics/android-opengl-box2d-basics-read-only/gen/com/lmdig/android/tutorial/oglbox2dbasics/R.java \
+ : /Users/bridgethillyer/learning/android/box2d-basics/android-opengl-box2d-basics-read-only/res/drawable-hdpi/icon.png \
+/Users/bridgethillyer/learning/android/box2d-basics/android-opengl-box2d-basics-read-only/res/drawable-ldpi/icon.png \
+/Users/bridgethillyer/learning/android/box2d-basics/android-opengl-box2d-basics-read-only/res/drawable-mdpi/icon.png \
+/Users/bridgethillyer/learning/android/box2d-basics/android-opengl-box2d-basics-read-only/res/layout/main.xml \
+/Users/bridgethillyer/learning/android/box2d-basics/android-opengl-box2d-basics-read-only/res/values/strings.xml \
+/Users/bridgethillyer/learning/android/box2d-basics/android-opengl-box2d-basics-read-only/bin/AndroidManifest.xml \
---
+++ gen/com/lmdig/android/tutorial/oglbox2dbasics/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.lmdig.android.tutorial.oglbox2dbasics;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
